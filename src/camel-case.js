19b6f7c52f07bbc9724d9636e5708f778a819d97
crochetcode <fred.davison@outlook.com>
2022-05-24 01:47:58 -0400

    :white_check_mark: toCamelCase

--- src/camel-case.js
+++ src/camel-case.js
@@ -2,13 +2,12 @@ const prepareString = str => str.replaceAll('-', '_');
 // const prepareString = str => str.replace(/-/g, '_');
 
 export const toCamelCase = str => {
-    const array = prepareString(str)
+    return prepareString(str)
     .split('_')
     .map((word, i) => {
         if (i > 0){
             return word.charAt(0).toUpperCase() + word.slice(1)
         } else return word;
-    });
-
-    return array.join('');
+    })
+    .join('');
 };
