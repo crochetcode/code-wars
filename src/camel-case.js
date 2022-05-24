2b97ec19782df4d722d7f9a6d34c8b3e8bdffbbb
crochetcode <fred.davison@outlook.com>
2022-05-24 01:45:09 -0400

    :judo: toCamelCase

--- src/camel-case.js
+++ src/camel-case.js
@@ -1,3 +1,14 @@
+const prepareString = str => str.replaceAll('-', '_');
+// const prepareString = str => str.replace(/-/g, '_');
+
 export const toCamelCase = str => {
-    return '';
+    const array = prepareString(str)
+    .split('_')
+    .map((word, i) => {
+        if (i > 0){
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        } else return word;
+    });
+
+    return array.join('');
 };
--- tests/camel-case.test.js
+++ tests/camel-case.test.js
@@ -4,4 +4,16 @@ describe('to camel case', () => {
     it('returns empty string', () => {
        expect(toCamelCase('')).toBe('');
     });
-})
+
+    it('converts snake_case to camelCase', () => {
+        expect(toCamelCase('example_number_one')).toBe('exampleNumberOne');
+    });
+
+    it('converts kebob-case to PascalCase', () => {
+        expect(toCamelCase('Example-Number-Two')).toBe('ExampleNumberTwo');
+    });
+
+    it('converts snake_case to PascalCase', () => {
+        expect(toCamelCase('A-B-C')).toBe('ABC');
+    });
+});
