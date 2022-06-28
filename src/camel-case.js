cd3b60edd85c2ae1435aa50b9fceff4daffb3abe
crochetcode <fred.davison@outlook.com>
2022-06-27 19:46:45 -0500

    :truck: move descriptions into src files

--- src/camel-case.js
+++ src/camel-case.js
@@ -1,3 +1,9 @@
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+The first word within the output should be capitalized only if the original word was capitalized
+(known as Upper Camel Case, also often referred to as Pascal case).
+*/
+
 const prepareString = str => str.replaceAll('-', '_');
 // const prepareString = str => str.replace(/-/g, '_');
 
